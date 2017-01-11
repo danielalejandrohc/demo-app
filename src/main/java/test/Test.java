9f3d2ada7bf82cb75176c4a5ee3fb8f6dcebab91
Daniel Hernandez <daniel_hernandez@alsicorp.milady.local>
2017-01-11 15:12:41 -0600

    test tomcat 7

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author daniel_hernandez
+ */
+@ManagedBean
+@ViewScoped
+public class Test {
+    
+    private String msg;
+
+    @PostConstruct
+    public void init() {
+        msg = "demo ...";
+    }
+    
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    
+    
+}
