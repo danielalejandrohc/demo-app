2b28ede9f43686806548a3b477937cd4b53b2350
Daniel Hernandez <daniel_hernandez@alsicorp.milady.local>
2017-01-12 15:19:08 -0600

    try jersey

---
+++ src/main/java/test/Service.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author daniel_hernandez
+ */
+@Path("demo")
+public class Service {
+    
+    @GET
+    @Path("/find")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response find(@PathParam("code") String code) {
+        return Response.ok("hola mundo").build();
+    }
+    
+}
